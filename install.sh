1191db3f29e20083fdee03ba0ac58e94caeb8f59
uqembeddedsys <50263708+uqembeddedsys@users.noreply.github.com>
2025-04-27 05:19:51 +1000

    adding files

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install libopencv-dev wget
+sudo apt install ffmpeg fswebcam v4l-utils
+sudo apt install imagemagick
+echo PATH=/usr/src/tensorrt/bin:$PATH >> ~/.bashrc
+source ~/.bashrc
+wget https://drive.google.com/file/d/1jMGb4xcdM5qmYR7EzKU1JBVGfxHQ-GMD/view?usp=drive_link
+wget https://drive.google.com/file/d/1C6A7G_Ro_gJVu29hlo-oDMyYAplFKWR6/view?usp=drive_link
+wget https://drive.google.com/file/d/1SBXfYHeb1Ccq41re4LyC4Y2NJaZMiJj4/view?usp=drive_link
+wget https://drive.google.com/file/d/1VaxD2R6FYvS-H71v0aEu3fnNU3jEPi7e/view?usp=drive_link
+wget https://drive.google.com/file/d/1xcHua9H8ykdU1ldYlqq6XXJQH48d4-4g/view?usp=drive_link
+wget https://drive.google.com/file/d/18NjgHl9Ndx9tcqoZp4MdQ7NLFniFU2O0/view?usp=drive_link
+wget https://drive.google.com/file/d/11E3Sw-btUUorB0XF5ETLxz8w4cejzA_M/view?usp=drive_link
+gunzip mnist_jpg_test.zip
